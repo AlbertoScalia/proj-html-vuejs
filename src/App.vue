37de9e20b2fb3e50ad0d39605c79af86c266efa7
AlbertoScalia <albscalia@gmail.com>
2023-05-30 15:17:06 +0200

    Feat: Add components and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderComponent/>
+  <MainComponent/>
+  <FooterComponent/>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
   export default {
-    name: 'App'
+  name: 'App',
+    components: {HeaderComponent, MainComponent, FooterComponent}
   }
 </script>
 
 <style lang="scss" scoped>
+
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <TopBar/>
+        <NavBar/>
+    </header>
+</template>
+
+<script>
+import TopBar from './TopBar.vue';
+import NavBar from './NavBar.vue';
+    export default {
+    name: 'HeaderComponent',
+    components: {
+        TopBar,
+            NavBar
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
