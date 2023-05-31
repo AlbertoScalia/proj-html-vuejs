bfa9c4272b4de4f556bb3a69a09b2437ca42e3d9
AlbertoScalia <albscalia@gmail.com>
2023-05-31 07:55:55 +0200

    Feat: Add Services and LearnAbout section

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,21 @@
 <template>
     <main>
         <Jumbotron/>
+        <Services/>
+        <LearnAbout/>
     </main>
 </template>
 
 <script>
 import Jumbotron from './Jumbotron.vue';
+import Services from './Services.vue';
+import LearnAbout from './LearnAbout.vue';
     export default {
     name: 'MainComponent',
     components: {
-            Jumbotron
+        Jumbotron,
+        Services,
+        LearnAbout
         }
     }
 </script>
